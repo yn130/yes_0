232fd4d7fc21e0eff8b14a95d4a964391f02c92a
yn130 <tjdpdms95@gmail.com>
2024-06-07 13:23:41 +0900

    프로젝트 시시작하기

--- lecture/practice/0.js
+++ lecture/practice/0.js
@@ -10,27 +10,58 @@ const icecreams = [
   '뮤! 넌 내거야',
   '오레오 쿠키 앤 크림',
 ];
-// for (let i = 0; i <9; i++){
-//   const img = document.createElement("img");
-//   img.src = `/quiz_javascript_dom/js_dom_quiz/image/icecream${i+1}.png`;
-//   img.classList.add("image-box");
+for (let i = 0; i <9; i++){
+  const img = document.createElement("img");
+  img.src = `./image/icecream${i+1}.png`;
+  img.classList.add("image-box");
 
-//   const h3 = document.createElement("h3");
-//   h3.classList.add('text-center');
-//   h3.textContent = `Top${i + 1}`;
+  const h3 = document.createElement("h3");
+  h3.classList.add('text-center');
+  h3.textContent = `Top${i + 1}`;
 
-//   const div = document.createElement("div");
-//   div.textContent = icecreams[i];
-//   div.classList.add("text-center");
+  const div = document.createElement("div");
+  div.textContent = icecreams[i];
+  div.classList.add("text-center");
 
-//   const article = document.createElement('article');
-//   article.classList.add("article-box");
-//   article.appendChild(img);
-//   article.appendChild(h3);
-//   article.appendChild(div);
+  const article = document.createElement('article');
+  article.classList.add("article-box");
+  article.appendChild(img);
+  article.appendChild(h3);
+  article.appendChild(div);
 
-//   document.querySelector("section").appendChild(article);
-// }
+  document.querySelector("section").appendChild(article);
+}
+
+
+    for (let i = 0; i < icecreams.length; i++) {
+        const img = document.createElement('img');
+        img.setAttribute('src', `./image/icecream${i + 1}.png`);
+        img.classList.add('img-box');
+
+        const topN = document.createElement('h3');
+        topN.textContent = `Top${i + 1}`;
+        topN.classList.add('text-center');
+
+        const name = document.createElement('div');
+        name.textContent = icecreams[i];
+        name.classList.add('text-center');
+
+        const article = document.createElement('article');
+        article.classList.add('article-box');
+        article.append(img);
+        article.append(topN);
+        article.append(name);
+
+        section.append(article);
+    }
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
 
 icecreams.forEach((val, idx) => {
     section.innerHTML += `
@@ -40,3 +71,6 @@ icecreams.forEach((val, idx) => {
     <div class = "text-center">${val}</div>
     </article>`
 });
+
+
+
