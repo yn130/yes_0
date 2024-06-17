95af7772355d87d4cf52a91c4b37976d40fd113e
yn130 <tjdpdms95@gmail.com>
2024-06-17 16:15:15 +0900

    ajax 공부하기

---
+++ lecture/11.FORM-dynamic/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('dynamic', { title: '동적 폼을 배워보자!' })
+})
+
+//ajax get 요청 처리 
+app.get('/ajax', (req,res)=>{
+    //GET 바익으로 브라우저에서 URL 퀴리까지 직접 입력해서도 값을 확인 가능
+    // ex. http://localhost:8080/ajax?name=션&gender=여자
+    console.log(req.query);
+    res.send(req.query);
+})
+//ajax post 요청 처리 
+app.post('/ajax', (req,res)=>{
+    console.log(req.body);
+    res.send(req.body);
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+})
