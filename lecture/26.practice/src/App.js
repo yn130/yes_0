55eee0141db7068fe6333ea66fd69d2c4d4a04f4
yn130 <tjdpdms95@gmail.com>
2024-08-07 14:34:53 +0900

    실습

---
+++ lecture/26.practice/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Prac2 from './Prac2';
+import Prac from './Prac';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Prac2 />
+      <hr />
+
+      <Prac />
+
+
+    </div>
+  );
+}
+
+export default App;
