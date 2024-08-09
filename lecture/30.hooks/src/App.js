60192426713ea47e88f52e2a979e88640ee02ac4
yn130 <tjdpdms95@gmail.com>
2024-08-09 18:59:32 +0900

    react hooks 학습하기

--- lecture/30.hooks/src/App.js
+++ lecture/30.hooks/src/App.js
@@ -2,8 +2,14 @@ import './App.css';
 import UseMemo from './components/UseMemo';
 import UseCallbackEx from './components/UseCallbackEx';
 import UseCallbackEx2 from './components/UseCallbackEx2';
+import UseReducerEx from './components/UseReducerEx';
+import useTitle from './hooks/useTitle';
+import Faq from './components/faq'
 
 function App() {
+
+  useTitle("React Hooks 학습중")
+
   return (
     <div className="App">
       <UseMemo />
@@ -13,6 +19,12 @@ function App() {
       <hr />
 
       <UseCallbackEx2 postId = "7"/> 
+      <hr />
+
+      <UseReducerEx />
+
+      <Faq />
+
 
     </div>
   );
