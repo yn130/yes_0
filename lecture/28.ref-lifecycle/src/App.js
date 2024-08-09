c24976f95d71895692e667ab8b77f9bc695b952f
yn130 <tjdpdms95@gmail.com>
2024-08-09 15:01:22 +0900

    react-life-cycle 공부하기

--- lecture/28.ref-lifecycle/src/App.js
+++ lecture/28.ref-lifecycle/src/App.js
@@ -2,6 +2,9 @@ import RefSample1 from "./RefSample1";
 import RefSample2 from "./RefSample2";
 import RefSample3 from "./RefSample3";
 import RefSample4 from "./RefSample4";
+import LifeCycleClass from "./LifeCycleClass"; 
+import LifeCycleFunc from "./LifeCycleFunc";
+
 
 function App() {
   return (
@@ -14,7 +17,13 @@ function App() {
       <hr />
       <RefSample4 />
 
+      <hr />
+      <LifeCycleClass /> 
 
+      <hr />
+      < LifeCycleFunc />
+
+      
 
     </div>
   );
