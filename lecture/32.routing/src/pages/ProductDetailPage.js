deea319f23e8a4c1d362f96c0e83d923c92609e4
yn130 <tjdpdms95@gmail.com>
2024-08-16 18:48:45 +0900

    react router 학습하기

--- lecture/32.routing/src/pages/ProductDetailPage.js
+++ lecture/32.routing/src/pages/ProductDetailPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { productInfos } from '../components/ProductList';
 
 export default function ProductDetailPage() {
     const { productId } = useParams();
     console.log('productId >>>>> ', productId); // '3'
     console.log(productInfos);
+
+    const navigate = useNavigate();
 
     const targetProduct = productInfos[Number(productId) - 1];
 
@@ -16,6 +18,8 @@ export default function ProductDetailPage() {
     return (
         <div>
             <h1>ProductDetailPage</h1>
+            <button onClick={() => navigate(-1)}> 뒤로가기 </button>
+            <button onClick={() => navigate('/')}> 홈으로 이동하기 </button>
             <ul>
                 <li>상품번호: {id}</li>
                 <li>상품명: {title}</li>
