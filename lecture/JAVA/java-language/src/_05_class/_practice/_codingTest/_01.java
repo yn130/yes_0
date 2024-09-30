cd26ecc24a0bf6977fd5b9a540bf73189197d361
yn130 <tjdpdms95@gmail.com>
2024-09-30 17:59:32 +0900

    wrapper 공부하기

---
+++ lecture/JAVA/java-language/src/_05_class/_practice/_codingTest/_01.java
@@ -0,0 +1,37 @@
+package _05_class._practice._codingTest;
+
+public class _01 {
+
+
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {2, 5, 8, 3, 7};
+
+    }
+}
+
+
+//
+//public class SumOfSmallerNumbers {
+//
+//    public static int sumSmallerNumbers(int[] arr, int target) {
+//        int sum = 0;
+//
+//        for (int num : arr) {
+//            if (num < target) {
+//                sum += num;
+//            }
+//        }
+//
+//        return sum;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] numbers = {10, 5, 3, 7, 8, 2};
+//        int target = 6;
+//
+//        int result = sumSmallerNumbers(numbers, target);
+//        System.out.println("Sum of numbers smaller than " + target + ": " + result);
+//    }
+//}
